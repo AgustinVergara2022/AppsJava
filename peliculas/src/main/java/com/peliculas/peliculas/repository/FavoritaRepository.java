1832a186d76151af72d5d7c68002c00cce96de0d
Agustin Vergara <111464395+AgustinVergara2022@users.noreply.github.com>
2025-10-08 13:00:44 -0300

    Delete peliculas directory

--- peliculas/src/main/java/com/peliculas/peliculas/repository/FavoritaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.peliculas.peliculas.repository;
-
-import com.peliculas.peliculas.entity.Favorita;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoritaRepository extends JpaRepository<Favorita, Long> {
-
-}
